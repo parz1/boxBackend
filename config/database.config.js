5e7246f2b47e19580327f7627a128d171b531dce
parz1 <1062134330@qq.com>
2019-07-13 10:55:08 +0800

    Initial structure.

---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'localhost',
+    port: 3306,
+    database: 'box',
+    user: 'root',
+    password: '123'
+};
---
+++ utils/db.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql");
+var databaseConfig = require("../config/database.config.js");
+
+module.exports = {
+  query: (sql, params, cb) => {
+    var conn = mysql.createConnection(databaseConfig);
+    try {
+      conn.connect(err => {
+        if (err) {
+          console.log("CONNECTION FAILED!");
+          throw err;
+        }
+        conn.query(sql, params, (err, res, fields) => {
+          if (err) {
+            console.log("SQL ERR!");
+            throw err;
+          }
+          cb && cb(res, fields); //res 为数据操作返回结果, fields为数据库连接的字段
+          conn.end(err => {
+            if (err) {
+              console.log("SHUTDOWN CONNECTION FAILED!");
+              throw err;
+            }
+          });
+        });
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+};
